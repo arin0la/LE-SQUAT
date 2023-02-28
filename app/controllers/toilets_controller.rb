2026367edeced54daa8462c2a74d8df12cf67790
Ariana Nelson <arinola540@gmail.com>
2023-02-28 10:05:18 +0000

    toilet index and toilet show

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,5 +1,11 @@
 class ToiletsController < ApplicationController
+  before_action create:
+
   def show
+    @toilet = Toilet.find(params[:id])
+  end
+
+  def index
     @toilets = Toilet.all
   end
 
@@ -9,11 +15,15 @@ class ToiletsController < ApplicationController
 
   def create
     @toilet = Toilet.new(toilet_params)
-    @toilet.save
+    @toilet.save!
     redirect_to toilet_path(@toilet)
   end
 
   private
+
+  def set_toilet_id
+    @toilet = Toilet.find(params[:id])
+  end
 
   def toilet_params
     params.require(:toilet).permit(:name, :description, :price)
