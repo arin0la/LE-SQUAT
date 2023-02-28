0f3a83793046a7773c91df6fa4d011b84f1da941
jerome junkeereeket <jjunkeereeket11@gmail.com>
2023-02-28 12:06:27 +0000

    idk whats happening

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,10 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @toilet = Toilet.find(params[:toilet_id])
   end
+
+  def destroy
+    @booking = Booking.find(params[:toilet_id])
+    @review.destroy
+    redirect_to toilets_path(@review.toilet), status: :see_other
+  end
 end
