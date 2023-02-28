f3616646d508f120d44b023129976c26d685dd67
giovanni di stefano <giovdist@hotmail.co.uk>
2023-02-28 11:57:43 +0000

    booking create + booking new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,16 @@
 class BookingsController < ApplicationController
+  before_action :set_toilet, only: %i[new create]
+
   def new
     @booking = Booking.new
-    @toilet = Toilet.find(params[:toilet_id])
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    @booking.toilet = @toilet
+    @booking.user = current_user
+    @booking.save
+    redirect_to toilets_path(@toilet)
   end
 
   def destroy
@@ -9,5 +18,15 @@ class BookingsController < ApplicationController
     @booking.destroy
     redirect_to toilets_path, status: :see_other
     # redirect_to toilets_path(@booking.toilet), status: :see_other
+  end
+
+  private
+
+  def set_toilet
+    @toilet = Toilet.find(params[:toilet_id])
+  end
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
